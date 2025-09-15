258190f93e8efd07d3b1c25539fd2f9ed274e514
13578 <1357887092@qq.com>
2025-09-15 18:22:20 +0800

    04_

--- level1/p04_goldbach/main.c
+++ level1/p04_goldbach/main.c
@@ -1,6 +1,23 @@
-#include <printf.h>
+#include <stdio.h>
+#include <windows.h>
 
-int main() {
-    printf("hello world!\n");
+# define MAX 100
+
+char list[MAX-1];
+
+int main(void) {
+    for (int i = 2; i < MAX; i++) {
+        if (list[i-1]==0) {
+            for (int j = i*i; j < MAX; j+=i) {
+                list[j-1] = 1;
+            }
+        }
+
+    }
+    for (int i = 2; i < MAX; i++) {
+        if (list[i-1]==0) {
+            printf("%d\n", i);
+        }
+    }
     return 0;
 }
