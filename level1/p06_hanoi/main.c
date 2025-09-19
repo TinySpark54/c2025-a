d17657f18ca4c39e642d166dee0d13f59f827b93
13578 <1357887092@qq.com>
2025-09-19 15:28:48 +0800

    06finish

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,26 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+static int step = 0;
+
+void hanoi(int num,char source, char target, char through) {
+    if (num == 1) {
+        printf("    %c -> %c\n", source, target);
+        step++;
+    }
+    else {
+        hanoi(num - 1, source, through, target);
+        printf("    %c -> %c\n", source, target);
+        step++;
+        hanoi(num - 1, through, source, target);
+    }
+    }
 
 int main() {
-    printf("hello world!\n");
+    printf("请输入层数：");
+    int num ;
+    scanf_s("%d",&num);
+    hanoi(num, 'A', 'B', 'C');
+    printf("总步数：%d\n",step);
+    system("pause");
     return 0;
 }
--- level1/p07_maze/main.c
+++ level1/p07_maze/main.c
@@ -1,6 +1,32 @@
-#include <printf.h>
+#include <windows.h>
+#include <stdio.h>
+#define WIDTH  20
+#define HEIGHT 10
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+const char map[HEIGHT][WIDTH+1]={
+    "▨▩▧▤▥▣◀▶▲▼●　",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+};
+void gotoxy(int X, int Y)
+{
+    COORD scrn;
+    HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+    scrn.X = X; scrn.Y = Y;
+    SetConsoleCursorPosition(hOuput, scrn);
+}
+
+void COLOR_PRINT(const char* s, int color)
+{
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | color);
+    printf(s);
+    SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | 7);
 }
