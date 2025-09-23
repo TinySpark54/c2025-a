9a6e88b890ac63d3fba3ce29a90acd8755d2968c
13578 <1357887092@qq.com>
2025-09-23 20:08:54 +0800

    09finish

--- level1/p07_maze/main.c
+++ level1/p07_maze/main.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #define WIDTH  20
 #define HEIGHT 10
+#define BEGIN_X 1
+#define BEGIN_Y 2
+#define END_X 3
+#define END_Y 4
 
 const char map[HEIGHT][WIDTH+1]={
     "▨▩▧▤▥▣◀▶▲▼●　",
@@ -15,18 +19,25 @@ const char map[HEIGHT][WIDTH+1]={
     "",
     ""
 };
-void gotoxy(int X, int Y)
+void gotoxy(int X, int Y ,HANDLE handle)
 {
     COORD scrn;
-    HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+
     scrn.X = X; scrn.Y = Y;
-    SetConsoleCursorPosition(hOuput, scrn);
+    SetConsoleCursorPosition(handle, scrn);
 }
 
-void COLOR_PRINT(const char* s, int color)
+void COLOR_PRINT(const char* s, int color ,HANDLE handle)
 {
-    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | color);
     printf(s);
     SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | 7);
+}
+
+int main() {
+    HANDLE h=GetStdHandle(STD_OUTPUT_HANDLE);
+    for(int i=0;i<HEIGHT;i++) {
+        printf(map[i]);
+    }
+    gotoxy(BEGIN_X,BEGIN_Y)
 }
